a7c65aeae140441061995d364be4ca023239cdb9
Filippo Tessarotto <zoeslam@gmail.com>
2020-10-20 13:39:10 +0200

    Secret from env

--- tests/decrypt_assets.sh
+++ tests/decrypt_assets.sh
@@ -2,20 +2,10 @@
 
 set -e
 
-if [ -n "$1" ]; then
-    password="$1"
-else
-    stty -echo
-    printf "Password: "
-    read password
-    stty echo
-    printf "\n"
-fi
-
 for file in TestAssets/*gpg
 do
     printf "Decrypting $file ... "
-    printf "%s" "$password" | gpg --decrypt --batch --passphrase-fd 0 --output "$(printf "%s" "$file" | sed 's/\.gpg$//')" "$file"
+    gpg --decrypt --batch --passphrase="$GPG_PASSPHRASE" --output "$(printf "%s" "$file" | sed 's/\.gpg$//')" "$file"
     rm "$file"
     printf "done\n"
 done
--- tests/encrypt_assets.sh
+++ tests/encrypt_assets.sh
@@ -2,16 +2,10 @@
 
 set -e
 
-stty -echo
-printf "Password: "
-read password
-stty echo
-printf "\n"
-
 for file in TestAssets/*
 do
     printf "Encrypting $file ... "
-    printf "%s" "$password" | gpg --batch --yes --passphrase-fd 0 --symmetric --armor --output "$file"".gpg" "$file"
+    gpg --batch --yes --passphrase="$GPG_PASSPHRASE" --symmetric --armor --output "$file"".gpg" "$file"
     rm "$file"
     printf "done\n"
 done
